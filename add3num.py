6006587cdda14977b9c3f34c0544a66312c18a19
Diamondbohara <boharadiamond39@gmail.com>
2025-05-04 15:10:45 +0545

    project

---
+++ add3num.py
@@ -0,0 +1,12 @@
+# Add three numbers in Python
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Calculate the sum
+total = num1 + num2 + num3
+
+# Display the result
+print("The sum of the three numbers is:", total)
