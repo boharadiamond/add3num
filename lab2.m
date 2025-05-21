a8e040086545d8d348cbe907f04fa44e6acc9301
boharadiamond <boharadiamond39@gmail.com>
2025-05-21 09:39:54 +0545

    updated matlab

--- lab2.m
+++ lab2.m
@@ -80,8 +80,57 @@ for i=1:h
 end
 
 subplot(3,1,3),imshow(y),title('Intensity Sliced Image with Preserving Background');
+%Bit plane slicing
+x=imread('lina.png');
+x=rgb2gray(x);
+id=double(x);
+c0=mod(id,2);
+c1=mod(floor(id/2),2);
+c2=mod(floor(id/4),2);
+c3=mod(floor(id/8),2);
+c4=mod(floor(id/16),2);
+c5=mod(floor(id/32),2);
+c6=mod(floor(id/64),2);
+c7=mod(floor(id/128),2);
+figure,imshow(x),title('original image');
+figure;
+subplot(3,3,1),imshow(c0),title('Bit Plane 0');
+subplot(3,3,2),imshow(c1),title('Bit Plane 1');
+subplot(3,3,3),imshow(c2),title('Bit Plane 2');
+subplot(3,3,4),imshow(c3),title('Bit Plane 3');
+subplot(3,3,5),imshow(c4),title('Bit Plane 4');
+subplot(3,3,6),imshow(c5),title('Bit Plane 5');
+subplot(3,3,7),imshow(c6),title('Bit Plane 6');
+subplot(3,3,8),imshow(c7),title('Bit Plane 7');
+recon=2*(2*(2*(2*(2*(2*(2*c7+c6)+c5)+c4)+c3)+c2)+c1)+c0;
+subplot(3,3,9),imshow(uint8(recon)),title('Reconstricted image');
 
-        
+%___________________________________________________
+%____________________Power Transformation _________________
+a=imread('lina.png');
+a=rgb2gray(a);
+c=im2double(a);
+s=c;
+s1=c;
+factor=220;
+[h,w]=size(a);
+for i=1:w
+    for j=1:w
+        s(i,j)=factor*log(1+c(i,j));%log transformation
+        s1(i,j)=factor*c(i,j)^(1/2);%power transformation
+    end
+end
+figure;subplot(3,1,1),imshow(a),title('original image');
+subplot(3,1,2),imshow(s/255),title('image after blog transformation');
+subplot(3,1,3),imshow(uint8(s1)),title('image after power transformation');
+
+    
+
+
+
+
+
+
         
        
 
